615396ad930e3b37bdc755b2a7d6566a44b024d6
Ellisande <justin.dragos@gmail.com>
2020-10-08 21:31:55 -0700

    Add win/lose

---
+++ public/lastPull.js
@@ -0,0 +1,17 @@
+let lastPullButton = document.getElementById("last_pull");
+let durgun = document.getElementById("durgun");
+let win = document.getElementById("win");
+let lose = document.getElementById("lose");
+lastPullButton.onclick = () => {
+  durgun.classList.remove("ded");
+  win.classList.add("hidden");
+  win.classList.add("hidden");
+  let tenMinutes = Date.now() % 600000; //10 minutes
+  let randomNumber = Math.random(tenMinutes) * 10;
+  if (randomNumber <= 4) {
+    win.classList.remove("hidden");
+    durgun.classList.add("ded");
+  } else {
+    lose.classList.remove("hidden");
+  }
+};
